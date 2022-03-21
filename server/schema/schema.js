c6f1e6dbc719a4913e338f06a53a8233fd93ba11
Hopper2021 <calico2021@gmail.com>
2022-03-21 08:24:21 -0500

    Add authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,9 +2,25 @@ const graphql = require('graphql');
 // lodash is used in the rootquery to help find books
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { 
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
 
+var books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
+];
 
+var authors = [
+    {name: 'Patrick Rothfuss', age: 44, id: '1'},
+    {name: 'Brandon Sanderson', age: 42, id: '2'},
+    {name: 'Terry Pratchett', age: 66, id: '3'},
+]
 
 // First you define types
 // second you define relationships
@@ -13,11 +29,21 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString } 
     })
 });
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt } 
+    })
+});
+
 
 // Root queries are entry points to your graph to grab any different data
 const RootQuery = new GraphQLObjectType({
@@ -27,7 +53,7 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             // defines what arguements are NEEDED and their types 
             // when searching for books in the schema
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args){
                 // This tells graphql how to data the data when someone makes a request
                 return _.find(books, { id: args.id });
